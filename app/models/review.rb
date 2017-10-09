cd145aae3fc6e99081e412170e154dcefe6a19d5
Gil Perez <perezgildardo@gmail.com>
2017-10-09 02:51:15 -0700

    included doctors_specialties and specialty to the serializer

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
 	# model association
-	belongs_to :doctor
+	belongs_to :user
 
 	# validations
 	validates_presence_of :rating
--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -3,4 +3,6 @@ class DoctorSerializer < ActiveModel::Serializer
   attributes :id, :doctor_id, :group_id, :name, :image, :address, :latitude, :longitude, :created_at, :updated_at
   # model association
   has_many :reviews
+  has_many :doctors_specialties
+  has_many :specialties
 end
