c0081c136725359aa26ed54d80ca9922b9832c6f
Gil Perez <perezgildardo@gmail.com>
2017-10-08 22:37:58 -0700

    user foreign_key created_by change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Model associations
-  has_many :doctors, foreign_key: :address
+  has_many :doctors, foreign_key: :created_by
   # Validations
   validates_presence_of :name, :email, :password_digest
 end
