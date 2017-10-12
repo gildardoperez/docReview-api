d246c2195b75101549389a3be4dbc31cf2eaa13b
Gil Perez <perezgildardo@gmail.com>
2017-10-11 23:15:56 -0700

    included counter to seralizer

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,6 +1,6 @@
 class DoctorSerializer < ActiveModel::Serializer
   # attributes to be serialized  
-  attributes :id, :doctor_id, :group_id, :name, :image, :address, :latitude, :longitude, :created_at, :updated_at
+  attributes :id, :doctor_id, :group_id, :name, :image, :address, :latitude, :longitude, :created_at, :reviews_count, :updated_at
   # model association
   has_many :reviews
   has_many :doctors_specialties
